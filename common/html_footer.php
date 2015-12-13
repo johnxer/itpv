20f56ddd233d1683a8baa2224cdf7062c281cda2
Jan Filas <johnxer@gmail.com>
2015-12-13 21:10:17 +0100

    dsdd

--- common/html_footer.php
+++ common/html_footer.php
@@ -746,6 +746,11 @@ $(document).ready(function(){
 });
 
 
+function isValidEmailAddress(emailAddress) {
+    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    return pattern.test(emailAddress);
+};
+
  function validate_enterpriseForm()
   {
     //console.log('xxx');
@@ -762,7 +767,7 @@ $(document).ready(function(){
         }
     })
 
-    if (all_good ) {
+    if (all_good && isValidEmailAddress($("#enterprise_email").val())) {
       $('.form_enterprise_submit').removeClass('disabled');
       //$('.important_lm').hide();
     }
@@ -785,7 +790,7 @@ $(document).ready(function(){
         }
     })
 
-    if (all_good ) {
+    if (all_good && isValidEmailAddress($("#entrepreneur_email").val())) {
       $('.form_entrepreneur_submit').removeClass('disabled');
       //$('.important_lm').hide();
     }
@@ -809,7 +814,7 @@ $(document).ready(function(){
         }
     })
 
-    if (all_good ) {
+    if (all_good && isValidEmailAddress($("#angel_email").val())) {
       $('.form_angel_submit').removeClass('disabled');
       //$('.important_lm').hide();
     }
@@ -833,7 +838,7 @@ $(document).ready(function(){
         }
     })
 
-    if (all_good ) {
+    if (all_good && isValidEmailAddress($("#vc_email").val())) {
       $('.form_vc_submit').removeClass('disabled');
       //$('.important_lm').hide();
     }
@@ -855,7 +860,7 @@ $(document).ready(function(){
         }
     })
 
-    if (all_good ) {
+    if (all_good && isValidEmailAddress($("#contact_email").val())) {
       $('.form_contact_submit').removeClass('disabled');
       //$('.important_lm').hide();
     }
@@ -877,7 +882,7 @@ $(document).ready(function(){
         }
     })
 
-    if (all_good ) {
+    if (all_good && isValidEmailAddress($("#hiring_email").val())) {
       $('.form_hiring_submit').removeClass('disabled');
       //$('.important_lm').hide();
     }
@@ -900,7 +905,7 @@ $(document).ready(function(){
         }
     })
 
-    if (all_good ) {
+    if (all_good && isValidEmailAddress($("#resources_email").val())) {
       $('.form_resources_submit').removeClass('disabled');
       //$('.important_lm').hide();
     }
@@ -922,7 +927,7 @@ $(document).ready(function(){
         }
     })
 
-    if (all_good ) {
+    if (all_good && isValidEmailAddress($("#mentor_email").val())) {
       $('.form_mentor_submit').removeClass('disabled');
       //$('.important_lm').hide();
     }
@@ -944,7 +949,7 @@ function validate_demoForm()
         }
     })
 
-    if (all_good ) {
+    if (all_good && isValidEmailAddress($("#demo_email").val())) {
       $('.form_demo_submit').removeClass('disabled');
       //$('.important_lm').hide();
     }
